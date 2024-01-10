da53035ce99fb2133492dccb9a5da9e71061f5ad
X-ERR0R-X <duartegomes@ipvc.pt>
2024-01-10 22:43:08 +0000

    .

--- routes/frota.js
+++
@@ -1,9 +0,0 @@
-const frotaRouter = require('express').Router();
-const controller = require('../controllers/frota');
-
-frotaRouter.get('/:id', controller.getById); //id categoria
-frotaRouter.post('/create', controller.create); //create new categoria
-frotaRouter.put('/update/:id', controller.update); //update categoria
-frotaRouter.delete('/delete/:id', controller.delete); //delete categoria
-
-module.exports = frotaRouter;
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-
-const lojasRouter = require('./lojas');
-const frotaRouter = require('./frota');
-
-router.use('/lojas', lojasRouter);
-router.use('/frota', frotaRouter);
-
-module.exports = router;
